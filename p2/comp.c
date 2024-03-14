46f7296e05c30aad398588dfa776d0bc8e9f11e9
Pablo Manzanares López <135281231+pey-45@users.noreply.github.com>
2024-03-14 17:48:35 +0100

    Update comp.c

--- p2/comp.c
+++ p2/comp.c
@@ -1,4 +1,3 @@
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -6,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
+#include <pthread.h>
 #include "compress.h"
 #include "chunk_archive.h"
 #include "queue.h"
@@ -14,41 +14,99 @@
 #define CHUNK_SIZE (1024*1024)
 #define QUEUE_SIZE 20
 
-#define COMPRESS 1
-#define DECOMPRESS 0
-
+typedef struct shared {
+    queue in, out;
+    archive ar;
+    int chunks, fd;
+    struct options *opt;
+} shared_t;
 
 // take chunks from queue in, run them through process (compress or decompress), send them to queue out
-void worker(queue in, queue out) {
+void* worker(void * ptr) {
+    shared_t *shared = ptr;
     chunk ch, res;
-    while(q_elements(in)>0) {
-        ch = q_remove(in);
+
+    while(q_elements(shared->in) > 0) {
+        ch = q_remove(shared->in);
 
         res = zcompress(ch);
         free_chunk(ch);
 
-        q_insert(out, res);
+        q_insert(shared->out, res);
     }
+
+    return (void*)0;
+}
+
+void* readChunks(void * ptr) {
+    shared_t *shared = ptr;
+    chunk ch;
+    int i, offset;
+
+    for(i=0; i<shared->chunks; i++) {
+        ch = alloc_chunk(shared->opt->size);
+
+        offset = (int)lseek(shared->fd, 0, SEEK_CUR);
+
+        ch->size = (int)read(shared->fd, ch->data, shared->opt->size);
+        ch->num = i;
+        ch->offset = offset;
+
+        q_insert(shared->in, ch);
+    }
+
+    return (void*)0;
+}
+
+void* writeChunks(void * ptr) {
+    shared_t *shared = ptr;
+    chunk ch;
+    int i;
+
+    for(i=0; i<shared->chunks; i++) {
+        ch = q_remove(shared->out);
+
+        add_chunk(shared->ar, ch);
+        free_chunk(ch);
+    }
+
+    return (void*)0;
+}
+
+void startThreads(shared_t * shared, struct options opt) {
+    pthread_t threads[opt.num_threads]; //hilos para worker
+    pthread_t thread_read; //hilo para lectura de chunks
+    pthread_t thread_write; //hilo para escritura de chunks
+    int i;
+    
+    pthread_create(&thread_read, NULL, readChunks, shared);
+    for (i = 0; i < opt.num_threads; i++) {
+        pthread_create(&threads[i], NULL, worker, shared);
+    }
+    pthread_create(&thread_write, NULL, writeChunks, shared);
+
+    pthread_join(thread_read, NULL);
+    for (i = 0; i < opt.num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    pthread_join(thread_write, NULL);
 }
 
 // Compress file taking chunks of opt.size from the input file,
 // inserting them into the in queue, running them using a worker,
 // and sending the output from the out queue into the archive file
 void comp(struct options opt) {
-    int fd, chunks, i, offset;
     char comp_file[256];
     struct stat st;
-    archive ar;
-    queue in, out;
-    chunk ch;
+    shared_t shared;
 
-    if((fd=open(opt.file, O_RDONLY))==-1) {
+    if((shared.fd=open(opt.file, O_RDONLY))==-1) {
         printf("Cannot open %s\n", opt.file);
         exit(0);
     }
 
-    fstat(fd, &st);
-    chunks = st.st_size/opt.size+(st.st_size % opt.size ? 1:0);
+    fstat(shared.fd, &st);
+    shared.chunks = (int)st.st_size/opt.size+(st.st_size % opt.size ? 1:0);
 
     if(opt.out_file) {
         strncpy(comp_file,opt.out_file,255);
@@ -57,39 +115,17 @@ void comp(struct options opt) {
         strncat(comp_file, ".ch", 255);
     }
 
-    ar = create_archive_file(comp_file);
+    shared.ar = create_archive_file(comp_file);
+    shared.in  = q_create(opt.queue_size);
+    shared.out = q_create(opt.queue_size);
+    shared.opt = &opt;
 
-    in  = q_create(opt.queue_size);
-    out = q_create(opt.queue_size);
+    startThreads(&shared, opt);
 
-    // read input file and send chunks to the in queue
-    for(i=0; i<chunks; i++) {
-        ch = alloc_chunk(opt.size);
-
-        offset=lseek(fd, 0, SEEK_CUR);
-
-        ch->size   = read(fd, ch->data, opt.size);
-        ch->num    = i;
-        ch->offset = offset;
-
-        q_insert(in, ch);
-    }
-
-    // compression of chunks from in to out
-    worker(in, out);
-
-    // send chunks to the output archive file
-    for(i=0; i<chunks; i++) {
-        ch = q_remove(out);
-
-        add_chunk(ar, ch);
-        free_chunk(ch);
-    }
-
-    close_archive_file(ar);
-    close(fd);
-    q_destroy(in);
-    q_destroy(out);
+    close_archive_file(shared.ar);
+    close(shared.fd);
+    q_destroy(shared.in);
+    q_destroy(shared.out);
 }
 
 
@@ -104,7 +140,7 @@ void decomp(struct options opt) {
     if((ar=open_archive_file(opt.file))==NULL) {
         printf("Cannot open archive file\n");
         exit(0);
-    };
+    }
 
     if(opt.out_file) {
         strncpy(uncomp_file, opt.out_file, 255);
@@ -136,7 +172,7 @@ void decomp(struct options opt) {
 int main(int argc, char *argv[]) {
     struct options opt;
 
-    opt.compress    = COMPRESS;
+    opt.compress    = 1;
     opt.num_threads = 3;
     opt.size        = CHUNK_SIZE;
     opt.queue_size  = QUEUE_SIZE;
@@ -144,6 +180,6 @@ int main(int argc, char *argv[]) {
 
     read_options(argc, argv, &opt);
 
-    if(opt.compress == COMPRESS) comp(opt);
+    if(opt.compress) comp(opt);
     else decomp(opt);
 }
