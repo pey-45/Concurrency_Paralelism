a60e645fd6ca9a9fbb86f115a99ba7ebd92ebe62
Pablo Manzanares López <135281231+pey-45@users.noreply.github.com>
2024-03-14 17:49:51 +0100

    Update comp.erl

--- p3/comp.erl
+++ p3/comp.erl
@@ -1,21 +1,24 @@
 -module(comp).
 
--export([comp/1, comp/2 , decomp/1, decomp/2]).
-
+%% Funciones visibles por otros archivos, con su cantidad de argumentos
+-export([comp_proc/2, comp_proc/3 , decomp_proc/2, decomp_proc/3]).
 
 -define(DEFAULT_CHUNK_SIZE, 1024*1024).
 
-%%% File Compression
+%% File Compression
+comp_proc(File, Procs) ->
+    comp_proc(File, ?DEFAULT_CHUNK_SIZE, Procs).
 
-comp(File) -> %% Compress file to file.ch
-    comp(File, ?DEFAULT_CHUNK_SIZE).
-
-comp(File, Chunk_Size) ->  %% Starts a reader and a writer which run in separate processes
+comp_proc(File, Chunk_Size, Procs) ->  %% Inicializa un Reader y un Writer que corren en procesos separados
     case file_service:start_file_reader(File, Chunk_Size) of
-        {ok, Reader} ->
+        {ok, Reader} -> %% Se inicia el proceso de lectura
             case archive:start_archive_writer(File++".ch") of
-                {ok, Writer} ->
-                    comp_loop(Reader, Writer);
+                {ok, Writer} -> %% Se inicia el proceso de escritura
+                    comp_loop(Reader, Writer),
+                    case Procs of
+                        0 -> ok;
+                        _ -> comp_proc(File, Chunk_Size, Procs-1)
+                    end;
                 {error, Reason} ->
                     io:format("Could not open output file: ~w~n", [Reason])
             end;
@@ -23,14 +26,14 @@ comp(File, Chunk_Size) ->  %% Starts a reader and a writer which run in separate
             io:format("Could not open input file: ~w~n", [Reason])
     end.
 
-comp_loop(Reader, Writer) ->  %% Compression loop => get a chunk, compress it, send to writer
-    Reader ! {get_chunk, self()},  %% request a chunk from the file reader
+comp_loop(Reader, Writer) ->  %% Bucle de compresion => coger un chunk, comprimirlo, y enviarselo al Writer
+    Reader ! {get_chunk, self()},  %% El proceso de lectura solicita un chunk
     receive
-        {chunk, Num, Offset, Data} ->   %% got one, compress and send to writer
-            Comp_Data = compress:compress(Data),
-            Writer ! {add_chunk, Num, Offset, Comp_Data},
-            comp_loop(Reader, Writer);
-        eof ->  %% end of file, stop reader and writer
+        {chunk, Num, Offset, Data} ->   %% Recibe un chunk, lo comprime, y se lo envia al Writer
+            Comp_Data = compress:compress(Data), %% Se almacena el archivo comprimido en Comp_Data
+            Writer ! {add_chunk, Num, Offset, Comp_Data}, %% El proceso de escritura recibe el chunk comprimido
+            comp_loop(Reader, Writer); %% Recursion
+        eof ->  %% Fin del archivo, se detienen el Reader y el Writer
             Writer ! stop;
         {error, Reason} ->
             io:format("Error reading input file: ~w~n",[Reason]),
@@ -39,16 +42,19 @@ comp_loop(Reader, Writer) ->  %% Compression loop => get a chunk, compress it, s
     end.
 
 %% File Decompression
+decomp_proc(Archive, Procs) ->
+    decomp_proc(Archive, string:replace(Archive, ".ch", "", trailing), Procs).
 
-decomp(Archive) ->
-    decomp(Archive, string:replace(Archive, ".ch", "", trailing)).
-
-decomp(Archive, Output_File) ->
+decomp_proc(Archive, Output_File, Procs) ->
     case archive:start_archive_reader(Archive) of
         {ok, Reader} ->
             case file_service:start_file_writer(Output_File) of
                 {ok, Writer} ->
-                    decomp_loop(Reader, Writer);
+                    decomp_loop(Reader, Writer),
+                    case Procs of
+                        0 -> ok;
+                        _ -> decomp_proc(Archive, Output_File, Procs - 1)
+                    end;
                 {error, Reason} ->
                     io:format("Could not open output file: ~w~n", [Reason])
             end;
@@ -71,3 +77,4 @@ decomp_loop(Reader, Writer) ->
             Writer ! abort,
             Reader ! stop
     end.
+
