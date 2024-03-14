62bb7a2fdce60e4b988e3ebb19a3342c1ffa8716
Pablo Manzanares López <135281231+pey-45@users.noreply.github.com>
2024-03-14 17:45:15 +0100

    Update queue.c

--- p2/queue.c
+++ p2/queue.c
@@ -1,4 +1,8 @@
 #include <stdlib.h>
+#include <pthread.h>
+#include <stdio.h>
+
+typedef struct _queue *queue;
 
 // circular array
 typedef struct _queue {
@@ -6,6 +10,9 @@ typedef struct _queue {
     int used;
     int first;
     void **data;
+    pthread_mutex_t mutex_queue;
+    pthread_cond_t empty;
+    pthread_cond_t full;
 } _queue;
 
 #include "queue.h"
@@ -18,6 +25,10 @@ queue q_create(int size) {
     q->first = 0;
     q->data  = malloc(size*sizeof(void *));
 
+    pthread_mutex_init(&q->mutex_queue, NULL);
+    pthread_cond_init(&q->empty, NULL);
+    pthread_cond_init(&q->full, NULL);
+
     return q;
 }
 
@@ -26,22 +37,39 @@ int q_elements(queue q) {
 }
 
 int q_insert(queue q, void *elem) {
-    if(q->size == q->used) return 0;
+
+    pthread_mutex_lock(&q->mutex_queue);
+    while (q->used == q->size) { //esta lleno
+        pthread_cond_wait(&q->full, &q->mutex_queue);
+    }
 
     q->data[(q->first+q->used) % q->size] = elem;
     q->used++;
+
+    if (q->used == 1) {
+        pthread_cond_broadcast(&q->empty);
+    }
+    pthread_mutex_unlock(&q->mutex_queue);
 
     return 1;
 }
 
 void *q_remove(queue q) {
     void *res;
-    if(q->used==0) return NULL;
+
+    pthread_mutex_lock(&q->mutex_queue);
+    while (!q->used) { //esta vacio
+        pthread_cond_wait(&q->empty, &q->mutex_queue);
+    }
 
     res = q->data[q->first];
-
     q->first = (q->first+1) % q->size;
     q->used--;
+
+    if (q->used == q->size - 1) {
+        pthread_cond_broadcast(&q->full);
+    }
+    pthread_mutex_unlock(&q->mutex_queue);
 
     return res;
 }
