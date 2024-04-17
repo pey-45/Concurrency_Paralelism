9f8ed2d1a2e3a621437a56e5672f79f18ac9340a
Pablo Manzanares López <135281231+pey-45@users.noreply.github.com>
2024-04-17 17:44:32 +0200

    Add files via upload

--- p6/p6.c
+++ p6/p6.c
@@ -2,8 +2,8 @@
 #include <sys/time.h>
 #include </usr/include/x86_64-linux-gnu/mpi/mpi.h>
 
-#define DEBUG 0
-#define N 81
+#define DEBUG 1
+#define N 9
 
 int ms(struct timeval * tv1, struct timeval * tv2) {
     return ((*tv2).tv_usec - (*tv1).tv_usec) + 1000000 * ((*tv2).tv_sec - (*tv1).tv_sec);
@@ -11,7 +11,7 @@ int ms(struct timeval * tv1, struct timeval * tv2) {
 
 int main(int argc, char *argv[]) {
 
-    int i, j, rank, numprocs, M;
+    int i, j, rank, numprocs, M, rest;
     float matrix[N][N], vector[N], result[N];
     struct timeval tv1, tv2;
     double work_ms, comm_ms;
@@ -28,25 +28,27 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     
-    int rest = N%numprocs;
+    M = N/numprocs;
+    rest = N%numprocs;
 
-    if (N <= numprocs) {
-        M = 1;
-    } else {
-        M = N/numprocs;
-        while (rest > 0) {
-            if (rank == rest-- - 1) M++;
+    int desplaz[numprocs], count[numprocs];
+    if (rank == 0) {
+        for (int i = 0; i < numprocs; i++) {
+            count[i] = i < rest? M + 1 : M;
+            desplaz[i] = i > 0? desplaz[i-1] + count[i-1] : 0;
         }
     }
 
-    printf("P%d: %d\n", rank, M);
+    
 
-    float work_matrix[M][N], work_result[M];
+    float work_matrix[count[rank]][N], work_result[count[rank]];
+
+    printf("P%d: %d\n", rank, M);
 
     gettimeofday(&tv1, NULL); // start
 
     // se envia la parte de la matriz que corresponde a cada proceso
-    MPI_Scatter(matrix, M*N, MPI_FLOAT, work_matrix, M*N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(matrix, count, desplaz, MPI_FLOAT, work_matrix, M*N, MPI_FLOAT, 0, MPI_COMM_WORLD);
     MPI_Bcast(vector, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
     gettimeofday(&tv2, NULL); // end
@@ -68,7 +70,7 @@ int main(int argc, char *argv[]) {
 
     gettimeofday(&tv1, NULL); // start
 
-    MPI_Gatherv(work_result, M, MPI_FLOAT, result, M, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Gather(work_result, M, MPI_FLOAT, result, M, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
     gettimeofday(&tv2, NULL); // end
 
